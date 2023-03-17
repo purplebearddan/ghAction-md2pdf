8865717951f91a313b3b0991d5a2da3b7347d382
purplebearddan <daniel_m@purplebeard.co.uk>
2023-03-17 21:28:03 +0000

    added basdir for potential image fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,6 +11,6 @@ mkdir -p "$GITHUB_WORKSPACE/out"
 echo "Generating PDF"
 
 # cp $GITHUB_WORKSPACE/* /app/input
-python3 /app/script.py | md-to-pdf --launch-options '{ "args": ["--no-sandbox"] }' > $FILE_PATH
+python3 /app/script.py | md-to-pdf --launch-options '{ "args": ["--no-sandbox"] }' --basedir $GITHUB_WORKSPACE > $FILE_PATH
 echo "DONE! - $FILE_PATH"
 echo "FILE_PATH=$FILE_PATH" >> $GITHUB_ENV
