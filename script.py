8c56e6eac69fdaee4b7de860e59313745a9d7f3a
purplebearddan <daniel_m@purplebeard.co.uk>
2023-03-17 20:08:45 +0000

    changed py to use regex

--- script.py
+++ script.py
@@ -1,9 +1,11 @@
 # imports
 import os
 from pathlib import Path
+import re
 
 directory: str = os.environ.get("GITHUB_WORKSPACE") # Directiory for crawling
 headingColor: str = "#AF2CFF" # Colour of headings
+_to_esc = re.compile(r'\s|[]()[]')
 
 
 # methods
@@ -16,16 +18,22 @@ def fileFinder(path: str, extension: str = 'md'):
         files.append(path)
     return files
 
+def escaper(match):
+    return '\\'+match.group(0)
+
+def escapeFilename(filename):
+    return _to_esc.sub(escaper, filename)
+
 
 def filenameCleaner(filename: str):
     """
     cleans the file name to just show the folder structure.\n
     Escapes brackets, removes the gh action directory and the .md file extension 
     """
-    # brackets
-    filename = filename.replace('(', '\(')
-    filename = filename.replace(')', '\)')
-    
+    # # brackets
+    # filename = filename.replace("(", "\(")
+    # filename = filename.replace(")", "\)")
+    filename = escapeFilename(filename)
     # gh action input directory
     filename = filename.replace(f'{directory}/', "")
     
@@ -34,11 +42,13 @@ def filenameCleaner(filename: str):
 
     return filename
 
+
+
 def main():
     # import various values for branding
     companyName: str = os.environ.get("COMPANYNAME") or "Purple Beard Training"
     courseName: str = os.environ.get("COURSENAME") or "Frontend Development"
-    
+
     if companyName:
         # brand the first page
         os.system(f'echo \'<h1 style="color: #63028f">{companyName}</h1>\'')
@@ -49,8 +59,8 @@ def main():
 
     for filename in fileFinder(directory):
         presentableFilename = filenameCleaner(filename)
-        os.system(f'echo "\n\n<em> {presentableFilename}</em>\n"')
-        os.system(f'cat "{filename}"')
+        os.system(f'echo "\n\n<em>{presentableFilename}</em>\n"')
+        os.system(f'cat "{escapeFilename(filename)}"')
 
 if __name__ == "__main__":
     main()
