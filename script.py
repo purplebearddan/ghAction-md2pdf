9ec83483cb35c4deb50f373cead361dd0e4eda63
purplebearddan <daniel_m@purplebeard.co.uk>
2023-03-17 20:51:55 +0000

    added sorting and gh action for releases

--- script.py
+++ script.py
@@ -7,7 +7,6 @@ import re
 
 directory: str = os.environ.get("GITHUB_WORKSPACE") # Directiory for crawling
 headingColor: str = "#AF2CFF" # Colour of headings
-_to_esc = re.compile(r'\s|[]()[]')
 
 
 # methods
@@ -18,33 +17,7 @@ def fileFinder(path: str, extension: str = 'md'):
     files = []
     for path in Path('.').rglob(f'*.{extension}'):
         files.append(path)
-    return files
-
-def escaper(match):
-    return '\\'+match.group(0)
-
-def escapeFilename(filename):
-    return _to_esc.sub(escaper, filename)
-
-
-def filenameCleaner(filename: str):
-    """
-    cleans the file name to just show the folder structure.\n
-    Escapes brackets, removes the gh action directory and the .md file extension 
-    """
-    # # brackets
-    # filename = filename.replace("(", "\(")
-    # filename = filename.replace(")", "\)")
-    filename = escapeFilename(filename)
-    # # gh action input directory
-    # filename = filename.replace(f'{directory}/', "")
-    
-    # # filename extension
-    # filename = filename.replace('.md', '')
-
-    return filename
-
-
+    return sorted(files, key=lambda x:float(re.findall("(\d+)",x)[0]))
 
 def main():
     # import various values for branding
@@ -56,12 +29,12 @@ def main():
         os.system(f'echo \'<h1 style="color: #63028f">{companyName}</h1>\'')
     if courseName:
         # add the course name
-        os.system(f'echo "<h2>{courseName}</2>\n"')
+        os.system(f'echo "<h2>{courseName}</h2>\n"')
 
 
     for filename in fileFinder(directory):
         # filename = filenameCleaner(filename)
-        os.system(f'echo "\n\n<em>{filename}</em>\n"')
+        os.system(f'echo "\n<em>{filename}</em>\n"')
         os.system(f'cat "{filename}"')
 
 if __name__ == "__main__":
