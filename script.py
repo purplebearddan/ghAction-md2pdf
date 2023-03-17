c2d228dc37973450f9c8b9c4db145a885d140d74
purplebearddan <daniel_m@purplebeard.co.uk>
2023-03-17 19:18:52 +0000

    changed py script to sort files properly

--- script.py
+++ script.py
@@ -11,9 +11,9 @@ def fileFinder(path: str, extension: str):
     Finds files in the specified path with the extension specified
     """
     files = []
-    for rootpath, _, filenames in os.walk(path):
+    for rootpath, _, filenames in os.walk(path, True):
         if filenames:
-            filenames.sort(reverse=True)
+            # filenames.sort(reverse=True)
             for f in filenames:
                 if f.endswith(f".{extension}"):
                     files.append(f'{rootpath}/{f}')
@@ -22,9 +22,19 @@ def fileFinder(path: str, extension: str):
 
 def filenameCleaner(filename: str):
     """
-    cleans the file name to just show the folder structure
+    cleans the file name to just show the folder structure.\n
+    Escapes brackets, removes the gh action directory and the .md file extension 
     """
-    filename: str = filename.replace('(', '\(').replace(')', '\)').replace(f'{directory}/', "").replace('.md', '')
+    # brackets
+    filename = filename.replace('(', '\(')
+    filename = filename.replace(')', '\)')
+    
+    # gh action input directory
+    filename = filename.replace(f'{directory}/', "")
+    
+    # filename extension
+    filename = filename.replace('.md', '')
+
     return filename
 
 def main():
@@ -32,8 +42,12 @@ def main():
     companyName: str = os.environ.get("COMPANYNAME") or "Purple Beard Training"
     courseName: str = os.environ.get("COURSENAME") or "Frontend Development"
     
-    # brand the first page
-    os.system(f'echo \'<h1 style="color: #63028f">{companyName}</h1>\n<h2> {courseName}</2>\n\n\'')
+    if companyName:
+        # brand the first page
+        os.system(f'echo \'<h1 style="color: #63028f">{companyName}</h1>')
+    if courseName:
+        # add the course name
+        os.system('<h2> {courseName}</2>\n\'')
 
 
     for filename in fileFinder(directory, "md"):
