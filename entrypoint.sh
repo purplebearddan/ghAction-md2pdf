ca688bc6588204de8090c27e510b6e799fd99cab
purplebearddan <daniel_m@purplebeard.co.uk>
2023-03-17 22:11:25 +0000

    rolled back to md-to-pdf

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,9 @@
 #!/bin/sh -l
 
 export FILE_PATH="$GITHUB_WORKSPACE/out/$PDF_FILE_NAME"
-export MD_FILE_PATH="$GITHUB_WORKSPACE/out/result.md"
 
-echo "installing pretty-markdown-pdf"
-npm install -g pretty-markdown-pdf
+echo "installing md-to-pdf"
+npm install -g md-to-pdf
 echo "creating output folder"
 mkdir -p "$GITHUB_WORKSPACE/out"
 
@@ -12,7 +11,6 @@ mkdir -p "$GITHUB_WORKSPACE/out"
 echo "Generating PDF"
 
 # cp $GITHUB_WORKSPACE/* /app/input
-python3 /app/script.py > "$MD_FILE_PATH"
-pretty-md-pdf -i $MD_FILE_PATH -o $FILE_PATH
+python3 /app/script.py | md-to-pdf --launch-options '{ "args": ["--no-sandbox"] }' --basedir "$GITHUB_WORKSPACE/" > $FILE_PATH
 echo "DONE! - $FILE_PATH"
 echo "FILE_PATH=$FILE_PATH" >> $GITHUB_ENV
