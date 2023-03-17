d71e598da686258af1403f1b0fbe9ef6ac5b5681
purplebearddan <daniel_m@purplebeard.co.uk>
2023-03-17 15:53:42 +0000

    added action env pass

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,3 +13,4 @@ echo "Generating PDF"
 # cp $GITHUB_WORKSPACE/* /app/input
 python3 /app/script.py | md-to-pdf --stylesheet "$PDF_STYLESHEET" --launch-options '{ "args": ["--no-sandbox"] }' > $FILE_PATH
 echo "DONE! - $FILE_PATH"
+echo "FILE_PATH=$FILE_PATH" >> $GITHUB_ENV
